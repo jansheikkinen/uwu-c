6448fabbbea8fb4e92d1e752776793afef1bed0a
jansheikkinen <jansheikkinen@mail.com>
2022-11-27 00:10:45 -0600

    owo what's this?

---
+++ main.c
@@ -0,0 +1,10 @@
+// main.c
+
+#include <stdio.h>
+
+#include "uwu.h"
+
+int main(void) owo whats this
+  pwease pwintf("Hewwo,world! uwu\n");
+  wetewn 0;
+uwu
---
+++ uwu.h
@@ -0,0 +1,33 @@
+#ifndef UWU_H
+#define UWU_H
+
+// ### TYPES ### //
+#define doubwe   double
+#define fwoat    float
+#define wong     long
+#define chaw     char
+#define showt    short
+#define wegistew register
+#define vowatiwe volatile
+#define extewn   extern
+
+// ### KEYWORDS ### //
+#define bweak    break
+#define ewse     else
+#define fow      for
+#define defauwt  default
+#define wetewn   return
+#define whiwe    while
+
+// ### SYMBOLS ### //
+#define owo      {
+#define uwu      }
+#define pwease /* */
+#define whats ;
+#define this ;
+
+// ### FUNCTIONS ### //
+#define pwintf printf
+
+
+#endif
